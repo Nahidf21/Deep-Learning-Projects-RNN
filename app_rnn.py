2b7ede19f1deeca02e2d0faee9effc89fb4f2bcc
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2024-09-20 00:09:05 -0500

    Add files via upload

---
+++ app_rnn.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf 
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+# Load the IMDB dataset word index 
+word_index = imdb.get_word_index()
+revers_word_index = {value: key for key, value in word_index.items()}
+
+#Load the model 
+model = load_model('simplernn.h5')
+
+
+# Function to preprocess user input
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+import streamlit as st
+## streamlit app
+# Streamlit app
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify it as positive or negative.')
+
+# User input
+user_input = st.text_area('Movie Review')
+
+if st.button('Classify'):
+
+    preprocessed_input=preprocess_text(user_input)
+
+    ## MAke prediction
+    prediction=model.predict(preprocessed_input)
+    sentiment='Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    # Display the result
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review.')
